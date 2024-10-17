8d8f42d73f6388d4b23676af5d40bc68efb87db6
SyedBashar <MhuammadBashar@gmail.com>
2024-10-17 17:05:15 +0500

    Done Task JS Scroe

---
+++ app.js
@@ -0,0 +1,16 @@
+var number = prompt("Enter Your score")
+if (number >= 90) {
+    alert("A")
+}
+else if (number >= 80) {
+    alert("B")
+}
+else if (number >= 70) {
+    alert("C")
+}
+else if (number >= 60) {
+    alert("D")
+}
+else {
+    alert("F")
+}
